769c6228bf89f6b4406474a30bfc65cb377f7f13
lwdgit <lwdggm@gmail.com>
2023-06-25 19:22:05 +0800

    feat: 调整示例

--- tests/telegram.ts
+++ tests/telegram.ts
@@ -2,6 +2,20 @@ import TelegramBot from 'node-telegram-bot-api';
 import chat from '..'
 import dotenv from 'dotenv';
 
+const throttle = (cb: Function, ms: number = 200) => {
+    let tid: NodeJS.Timeout | null;
+    let lastArgs: any[] = [];
+    return (...args) => {
+        lastArgs = args;
+        if (!tid) {
+            tid = setTimeout(() => {
+                tid = null;
+                cb(...lastArgs);
+            }, ms);
+        }
+    };
+}
+
 dotenv.config();
 
 const token = process.env.TELEGTRAM_BOT_TOKEN;
@@ -13,17 +27,14 @@ bot.onText(/(.+)/, async (msg, match) => {
     const msgText = match[1];
 
     const result = await bot.sendMessage(chatId, '请稍候...');
-    let lastLength = 0;
     async function sendMessage(msg) {
-        if (lastLength === msg.length || !msg.length) return;
-        lastLength = msg.length;
         bot.editMessageText(msg, {
             chat_id: chatId,
             message_id: result.message_id,
         });
     }
     const response = await chat(msgText, {
-        onMessage: sendMessage,
+        onMessage: throttle(sendMessage),
     });
     sendMessage(response);
 });
